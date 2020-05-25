95df71151ae8f0e3ba23176166a974a5e49f3579
Austin <aripang.com@gmail.com>
2020-05-26 06:16:32 +0800

    removed unused files

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,1 @@
-export { initGA, logPageView, logModalView, logEvent, logException } from './analytics'
-export { default as metaData } from './seo'
-export { backToHistory, cc_format, expired_format, formatBillingDate, getCookie, getToday, numberWithCommas } from './utility'
+
