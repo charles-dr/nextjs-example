62daf802fca8f81999f00a297fe405e846acea32
Austin <aripang.com@gmail.com>
2020-05-26 06:29:18 +0800

    minor

--- store/index.js
+++ store/index.js
@@ -8,13 +8,8 @@ const reducers = combineReducers({
   lesson: lessonReducer,
 })
 
-const reduxDevtools =
-  typeof window !== 'undefined' && process.env.NODE_ENV !== 'production'
-    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-    : f => f
 
-const allStoreEnhancers = compose(applyMiddleware(thunk), reduxDevtools) //
+const allStoreEnhancers = compose(applyMiddleware(thunk))
 
 const makeStore = () => {
   const store = createStore(reducers, allStoreEnhancers)
